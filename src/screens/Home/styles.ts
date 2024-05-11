44a3a790b08d706b299d36ad35c9f00081446e8a
santanaIgot <santanaigoor01@gmail.com>
2024-05-11 19:29:47 -0300

    [FIX] - CHANGING THE SIZE

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -19,12 +19,14 @@ export const styles = StyleSheet.create({
     },
 
     input:{
+      flex:1,
       height:56,
       backgroundColor:"#1f1e25",
       borderRadius:5,
       color:"#fdfcfe",
       padding:16,
-      fontSize:16
+      fontSize:16,
+      marginRight:12
     },
 
     buttonText:{
@@ -39,6 +41,13 @@ export const styles = StyleSheet.create({
       backgroundColor:'#31cf67',
       alignItems:'center',
       justifyContent: 'center'
+    },
+
+    form:{
+      width:'100%',
+      flexDirection:'row',
+      marginTop:36,
+      marginBottom:42
     }
 
 
